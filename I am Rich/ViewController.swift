d3cd2f1c86ba6eb4ab538fa76910ef8edef3a3d0
ligiaviana <ligia.viana.azevedo@gmail.com>
2022-04-20 10:21:24 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Lígia Viana Azevedo on 20/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
